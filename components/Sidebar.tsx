7faee59286df583d173b65cd0b325a3840124f7c
janusha sridhar <janushasridhar@janushas-MacBook-Pro.local>
2022-06-20 20:34:10 -0400

    twitter api setup and sign in set up

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,8 +10,10 @@ import {
   HomeIcon,
 } from "@heroicons/react/outline";
 import SidebarRow from "./SidebarRow";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
       <img className="m-3 h-10 w-10" src="./twitter-logo.png" alt="" />
@@ -21,7 +23,11 @@ function Sidebar() {
       <SidebarRow Icon={MailIcon} title="Messages" />
       <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
       <SidebarRow Icon={CollectionIcon} title="Lists" />
-      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow
+        onClick={session ? signOut : signIn}
+        Icon={UserIcon}
+        title={session ? "Sign Out" : "Sign In"}
+      />
 
       <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Tweet } from "../typings";
+import React, { useEffect, useState } from "react";
+import { Comment, Tweet } from "../typings";
 import TimeAgo from "react-timeago";
 import {
   ChatAlt2Icon,
@@ -7,12 +7,24 @@ import {
   SwitchHorizontalIcon,
   UploadIcon,
 } from "@heroicons/react/outline";
+import { fetchComments } from "../utils/fetchComments";
 
 interface Props {
   tweet: Tweet;
 }
 
 function Tweet({ tweet }: Props) {
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  const refreshComments = async () => {
+    const comments: Comment[] = await fetchComments(tweet._id);
+    setComments(comments);
+  };
+
+  useEffect(() => {
+    refreshComments();
+  }, []);
+  console.log("comments", comments);
   return (
     <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
       <div className="flex space-x-3">
@@ -47,7 +59,7 @@ function Tweet({ tweet }: Props) {
       <div className="flex justify-between mt-5">
         <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <ChatAlt2Icon className="h-5 w-5" />
-          <p>5</p>
+          <p>{comments.length}</p>
         </div>
         <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <SwitchHorizontalIcon className="h-5 w-5" />
@@ -59,6 +71,33 @@ function Tweet({ tweet }: Props) {
           <UploadIcon className="h-5 w-5" />
         </div>
       </div>
+      {comments?.length > 0 && (
+        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
+          {comments.map((comment) => (
+            <div key={comment._id} className="relative flex space-x-2">
+              <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+              <img
+                className="mt-2 h-7 w-7 object-cover rounded-full "
+                src={comment.profileImg}
+                alt=""
+              />
+              <div>
+                <div className="flex items-center space-x-1">
+                  <p className="mr-1 font-bold">{comment.username}</p>
+                  <p className="hidden text-sm text-gray-500 lg:inline">
+                    @{comment.username.replace(/\s+/g, "").toLowerCase()} ·{" "}
+                  </p>
+                  <TimeAgo
+                    className="text-sm text-gray-500"
+                    date={comment._createdAt}
+                  />
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
